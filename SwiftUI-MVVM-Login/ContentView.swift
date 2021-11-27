f5865a99cf5c3e5903dedfc427c2cf3077866fd8
Yusuke Miyata <yskmyt.dev@gmail.com>
2021-11-28 00:28:41 +0900

    Initial Commit

---
+++ SwiftUI-MVVM-Login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM-Login
+//
+//  Created by Yusuke Miyata on 2021/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-MVVM-Login/SwiftUI_MVVM_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_LoginApp.swift
+//  SwiftUI-MVVM-Login
+//
+//  Created by Yusuke Miyata on 2021/11/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
